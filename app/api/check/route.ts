f5d766a0e8078668246a3de701b021a3d3ed1d52
mercenarioz <funnystar320@gmail.com>
2024-11-15 15:56:12 +0700

    bug fix

--- app/api/check/route.ts
+++ app/api/check/route.ts
@@ -3,21 +3,20 @@ import Session from "@/app/models/Session";
 import { NextResponse } from "next/server";
 import ParticipantModel from "@/app/models/Participant";
 
+export const dynamic = "force-dynamic";
 export async function GET() {
-  await connectDb();
-
   // get all sessions
   try {
+    await connectDb();
     const allSessions = await Session.find({}).populate({
       path: "participants",
       model: ParticipantModel,
     });
 
+    console.log(allSessions);
+
     return NextResponse.json(allSessions, {
       status: 200,
-      headers: {
-        "Cache-Control": "no-store",
-      },
     });
   } catch (error) {
     console.log("something went wrong: ", error);
