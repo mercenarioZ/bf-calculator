84011725b88c7b06e8c91859ab45dd345258f992
mercenarioz <funnystar320@gmail.com>
2024-11-15 15:18:20 +0700

    fix: no-cache config for responses

--- app/api/check/route.ts
+++ app/api/check/route.ts
@@ -1,13 +1,12 @@
 import { connectDb } from "@/app/libs/db";
 import Session from "@/app/models/Session";
 import { NextResponse } from "next/server";
-import ParticipantModel, {
-  ParticipantDocument,
-} from "@/app/models/Participant";
+import ParticipantModel from "@/app/models/Participant";
+import { headers } from "next/headers";
 
 export async function GET() {
   await connectDb();
-  
+
   // get all sessions
   try {
     const allSessions = await Session.find({}).populate({
@@ -15,7 +14,12 @@ export async function GET() {
       model: ParticipantModel,
     });
 
-    return NextResponse.json(allSessions, { status: 200 });
+    return NextResponse.json(allSessions, {
+      status: 200,
+      headers: {
+        "Cache-Control": "no-store",
+      },
+    });
   } catch (error) {
     console.log("something went wrong: ", error);
   }
